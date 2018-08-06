fff6c320d13d191c00bd74af444d88bde1cfc515
Andres Ciceri <andresciceri@gmail.com>
2018-08-05 23:19:20 -0500

    Class: 6 Auth, List & search books Api Books, Favorites section

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   apiBooks: 'https://www.googleapis.com/books/v1/',
   firebase: {
-    apiKey: 'AIzaSyAC47nrHmeCqCmNUIcmBBIVcDiJR1_8Hzw',
-    authDomain: 'bzg-books-app.firebaseapp.com',
-    databaseURL: 'https://bzg-books-app.firebaseio.com/',
-    projectId: 'bzg-books-app'        
+    apiKey: 'AIzaSyCuHctOJyQAB8ynqwxZ3wg6QVrYW-hW9p0',
+    authDomain: 'bzg-books-app-2.firebaseapp.com',
+    databaseURL: 'https://bzg-books-app-2.firebaseio.com/',
+    projectId: 'bzg-books-app-2'        
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   production: false,
   apiBooks: 'https://www.googleapis.com/books/v1/',
   firebase: {
-    apiKey: 'AIzaSyAC47nrHmeCqCmNUIcmBBIVcDiJR1_8Hzw',
-    authDomain: 'bzg-books-app.firebaseapp.com',
-    databaseURL: 'https://bzg-books-app.firebaseio.com/',
-    projectId: 'bzg-books-app'        
+    apiKey: 'AIzaSyCuHctOJyQAB8ynqwxZ3wg6QVrYW-hW9p0',
+    authDomain: 'bzg-books-app-2.firebaseapp.com',
+    databaseURL: 'https://bzg-books-app-2.firebaseio.com/',
+    projectId: 'bzg-books-app-2'        
   }
 };
 
