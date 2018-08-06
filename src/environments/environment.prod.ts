993e53d8a0d71b9eacf12f419f6e770bff6e004b
Andres Ciceri <andresciceri@gmail.com>
2018-08-06 15:00:53 -0500

    Class: 6-Update Environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   apiBooks: 'https://www.googleapis.com/books/v1/',
   firebase: {
-    apiKey: 'AIzaSyCuHctOJyQAB8ynqwxZ3wg6QVrYW-hW9p0',
-    authDomain: 'bzg-books-app-2.firebaseapp.com',
-    databaseURL: 'https://bzg-books-app-2.firebaseio.com/',
-    projectId: 'bzg-books-app-2'        
+    apiKey: 'API-KEY',
+    authDomain: 'AUTH-DOMAIN',
+    databaseURL: 'DB-URL',
+    projectId: 'PROJECT-ID'        
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   production: false,
   apiBooks: 'https://www.googleapis.com/books/v1/',
   firebase: {
-    apiKey: 'AIzaSyCuHctOJyQAB8ynqwxZ3wg6QVrYW-hW9p0',
-    authDomain: 'bzg-books-app-2.firebaseapp.com',
-    databaseURL: 'https://bzg-books-app-2.firebaseio.com/',
-    projectId: 'bzg-books-app-2'        
+    apiKey: 'API-KEY',
+    authDomain: 'AUTH-DOMAIN',
+    databaseURL: 'DB-URL',
+    projectId: 'PROJECT-ID'        
   }
 };
 
